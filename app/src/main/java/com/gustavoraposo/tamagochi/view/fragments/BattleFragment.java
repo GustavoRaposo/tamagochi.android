3cbcd1657ed894ab31e7321b4587cb59f9057540
GustavoRaposo <gustavo_f.raposo@hotmail.com>
2021-10-29 00:08:16 -0300

    feat: add nav graphs and fragments

---
+++ app/src/main/java/com/gustavoraposo/tamagochi/view/fragments/BattleFragment.java
@@ -0,0 +1,25 @@
+package com.gustavoraposo.tamagochi.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gustavoraposo.tamagochi.R;
+
+public class BattleFragment extends Fragment {
+
+    public BattleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_battle, container, false);
+    }
+}
---
+++ app/src/main/java/com/gustavoraposo/tamagochi/view/fragments/SignInFragment.java
@@ -0,0 +1,39 @@
+package com.gustavoraposo.tamagochi.view.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gustavoraposo.tamagochi.R;
+
+public class SignInFragment extends Fragment {
+    public static SignInFragment newInstance(){
+        return new SignInFragment();
+    }
+
+    private NavController mNavController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mNavController = Navigation.findNavController(requireView());
+    }
+}
